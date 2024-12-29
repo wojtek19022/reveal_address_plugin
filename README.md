9cf189e8890a1bd4e8b18b80d4b61ecf52cf33ee
Wojciech Sołyga <113895660+wojtek19022@users.noreply.github.com>
2024-12-29 15:00:36 +0100

    Unifikacja wyglądu wtyczki
    - Dodanie loga wtyczki
    - Dodanie automatycznego przypisania wtyczki do toolbara ES
    - Dodanie translacji nagłówka wtyczki
    - Ulepszenie opisu wtyczki
    - Naprawienie wyświetlania ikony wtyczki w toolbarze
    - Podniesienie wersji wtyczki do 1.2.1

--- README.md
+++ README.md
@@ -1,17 +1,27 @@
-# Reveal Address Plugin
-This is a QGIS plugin that allows users to view the address of a location on the map by clicking on it. The plugin uses the Nominatim API to reverse geocode the coordinates of the clicked location to obtain its address. Plugin has been made with ChatGPT.
+# Reveal Address
 
-## Installation
-To install the plugin, follow these steps:
+## PL
+Wtyczka QGIS, która umożliwia użytkownikom wyświetlanie adresu wybranej lokalizacji na mapie. Wtyczka wykorzystuje interfejs API Nominatim do odwrotnego geokodowania współrzędnych klikniętej lokalizacji w celu uzyskania jej adresu. Wtyczka została stworzona przy użyciu ChatGPT.
 
-1. Open QGIS
-2. Go to Plugins > Manage and Install Plugins
-3. In the All tab, search for Reveal Address
-4. Click on Install Plugin
-## Usage
-To use the plugin, follow these steps:
+### Instrukcja pobrania
+* Wtyczkę należy zainstalować w QGIS jako ZIP lub wgrać pliki wtyczki do lokalizacji `C:\Users\User\AppData\Roaming\QGIS\QGIS3\profiles\default\python\plugins`.
+* Aby uruchomić wtyczkę, należy kliknąć na ikonę niebieskiego drzewa ze znakiem ładowania.
+* Jeżeli ikona wtyczki nie jest widoczna w panelu warstw, spróbuj zrestartować QGIS.
+* Jeżeli wtyczka nadal nie jest widoczna, należy przejść w QGIS Desktop do Wtyczki -> Zarządzanie wtyczkami -> Zainstalowane -> Reveal Address -> Odinstalować wtyczkę i zainstalować ponownie.
 
-1. After installing the plugin, click on the Reveal Address button in the toolbar to activate the map tool.
-1. Click on a location on the map to view its address in a message box.
-## Support
-For support, please contact us at office@envirosolutions.pl.
+### Instrukcja użytkowania:
+1. Po zainstalowaniu wtyczki kliknij przycisk wtyczki Reveal Address na górnym pasku narzędzi, aby aktywować narzędzie mapy.
+2. Kliknij lokalizację na mapie, aby wyświetlić jej adres w oknie wiadomości.
+
+## EN
+A QGIS plugin that allows users to view the address of a location on the map by clicking on it. The plugin uses the Nominatim API to reverse geocode the coordinates of the clicked location to obtain its address. Plugin has been made with ChatGPT.
+
+### Download Instructions
+* The plugin should be installed in QGIS as a ZIP or by uploading the plugin files to the location `C:\Users\User\AppData\Roaming\QGIS\QGIS3\profiles\default\python\plugins`.
+* To launch the plugin, click on the blue tree icon labeled with loading sign.
+* If the plugin icon is not visible in the layer panel, try restarting QGIS.
+* If the plugin is still not visible, go to QGIS Desktop -> Plugins -> Manage and Install Plugins -> Installed -> Reveal Address -> Uninstall the plugin and reinstall it.
+
+### Usage Instructions:
+1. After installing the plugin, click on the Reveal Address plugin button in the top toolbar to activate the map tool.
+2. Click on a location on the map to view its address in a message box.
--- RevealAddressPlugin.py
+++ RevealAddressPlugin.py
@@ -1,10 +1,16 @@
 from qgis.core import QgsNetworkAccessManager, QgsPointXY
 from qgis.gui import QgsMapToolEmitPoint, QgsMapTool, QgsMapCanvas
 from qgis.core import QgsCoordinateTransform, QgsCoordinateReferenceSystem
-from qgis.PyQt.QtWidgets import QMessageBox, QAction
+from qgis.PyQt.QtWidgets import QMessageBox, QAction, QToolBar
 from qgis.PyQt.QtNetwork import QNetworkRequest, QNetworkReply
-from qgis.PyQt.QtCore import QUrl
+from qgis.PyQt.QtCore import QUrl, QCoreApplication
+from qgis.PyQt.QtGui import QIcon
 import json
+import os
+
+"""Wersja wtyczki"""
+plugin_name = "Reveal address"
+plugin_version = "1.2.1"
 
 class RevealAddressMapTool(QgsMapToolEmitPoint):
     def __init__(self, canvas):
@@ -30,7 +36,10 @@ class RevealAddressMapTool(QgsMapToolEmitPoint):
 
         # Send the GET request and connect the finished signal to the self.handleResult() method
         reply = self.nam.get(req)
-        reply.finished.connect(self.handleResult)
+        result = reply.finished.connect(self.handleResult)
+
+        if result:
+            self.canvas.unsetMapTool(self)
 
     def handleResult(self):
         reply = self.sender()
@@ -43,33 +52,115 @@ class RevealAddressMapTool(QgsMapToolEmitPoint):
         else:
             address = "No address found"
 
-
         # Show the address in a message box
         QMessageBox.information(None, "Address", address)
-
+        
+        return True
 
 
 class RevealAddressPlugin:
     def __init__(self, iface):
         self.map_tool = None
         self.action = None
+        # Save reference to the QGIS interface
         self.iface = iface
+        # initialize plugin directory
+        self.plugin_dir = os.path.dirname(__file__)
+        self.icon_path = f'{self.plugin_dir}\icons\icon.svg'
 
+        self.actions = []
+        self.menu = u'&EnviroSolutions'
+        self.toolbar = self.iface.mainWindow().findChild(QToolBar, 'EnviroSolutions')
+        
+        if not self.toolbar:
+            self.toolbar = self.iface.addToolBar(u'EnviroSolutions')
+            self.toolbar.setObjectName(u'EnviroSolutions')
+            
+        self.shortcut = None
+        # Check if plugin was started the first time in current QGIS session
+        # Must be set in initGui() to survive plugin reloads
+        self.first_start = None
+
+    def add_action(
+                self,
+                icon_path,
+                text,
+                callback,
+                enabled_flag=True,
+                add_to_menu=True,
+                add_to_toolbar=True,
+                status_tip=None,
+                whats_this=None,
+                parent=None):
+
+            icon = QIcon(icon_path)
+            action = QAction(icon, text, parent)
+            action.triggered.connect(callback)
+            action.setEnabled(enabled_flag)
+
+            if status_tip is not None:
+                action.setStatusTip(status_tip)
+
+            if whats_this is not None:
+                action.setWhatsThis(whats_this)
+
+            if add_to_toolbar:
+                self.toolbar.addAction(action)
+
+            if add_to_menu:
+                self.iface.addPluginToMenu(
+                    self.menu,
+                    action)
+
+            self.actions.append(action)
+
+            return action
+
+    # noinspection PyMethodMayBeStatic
+    def tr(self, message):
+        """Get the translation for a string using Qt translation API.
+
+        We implement this ourselves since we do not inherit QObject.
+
+        :param message: String for translation.
+        :type message: str, QString
+
+        :returns: Translated version of message.
+        :rtype: QString
+        """
+        # noinspection PyTypeChecker,PyArgumentList,PyCallByClass
+        return QCoreApplication.translate('RevealAddressPlugin', message)
+    
     def initGui(self):
-        # Create action to activate the map tool
-        self.action = QAction("Reveal Address", self.iface.mainWindow())
-        self.action.triggered.connect(self.activate)
+        """Create the menu entries and toolbar icons inside the QGIS GUI."""
 
-        # Add the action to the toolbar
-        self.iface.addToolBarIcon(self.action)
+        self.add_action(
+            self.icon_path,
+            text=self.tr(u'Reveal Address'),
+            callback=self.run,
+            parent=self.iface.mainWindow()
+        )
+        
+        # will be set False in run()
+        self.first_start = True
 
-    def activate(self):
+    def run(self):
         # Create and set the map tool
         self.map_tool = RevealAddressMapTool(self.iface.mapCanvas())
         self.iface.mapCanvas().setMapTool(self.map_tool)
 
     def unload(self):
+        """Removes the plugin menu item and icon from QGIS GUI."""
+        for action in self.actions:
+            self.iface.removePluginMenu(
+                u'&EnviroSolutions',
+                action)
+            # self.iface.removeToolBarIcon(action)
+            self.toolbar.removeAction(action)
+                
         # Remove the map tool and action when the plugin is unloaded
         if self.map_tool:
             self.iface.mapCanvas().unsetMapTool(self.map_tool)
-        self.iface.removeToolBarIcon(self.action)
+
+        # remove the toolbar
+        del self.toolbar
